4a048873b5c3397f8280993058b3f94994255cb5
Juanjo128 <jchavesa@unal.edu.co>
2021-03-16 21:14:50 -0500

    Create Control.java

---
+++ src/main/java/taller3/televisores/Control.java
@@ -0,0 +1,44 @@
+package taller3.televisores;
+
+public class Control {
+private TV tv;
+	
+	Control(){
+		
+	}
+	
+	public void turnOn() {
+		tv.turnOn();
+	}
+	
+	public void turnOff() {
+		tv.turnOff();
+	}
+	
+	public void canalUp() {
+		tv.canalUp();
+	}
+	
+	public void canalDown() {
+		tv.canalDown();
+	}
+	
+	public void volumenUp() {
+		tv.volumenUp();
+	}
+	
+	public void volumenDown() {
+		tv.volumenDown();
+	}
+	
+	public void setCanal(int x) {
+		if(x >= 1 && x <= 120) {
+		tv.setCanal(x);
+		}
+	}
+	
+	public void enlazar(TV x) {
+		this.tv = x;
+		x.setControl(this);
+	}
+}
